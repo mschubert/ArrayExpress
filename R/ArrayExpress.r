c7b9283eae1c7d9ce9d925a053e1fb7c0424c7b9
d.tenenbaum <d.tenenbaum@bc3139a8-67e5-0310-9ffc-ced21a209358>
2015-04-16 20:00:01 +0000

    Creating branch for BioC 3.1 release.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/branches/RELEASE_3_1/madman/Rpacks/ArrayExpress@102593 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/ArrayExpress.r
@@ -0,0 +1,29 @@
+ArrayExpress = function(accession, path = getwd(), save = FALSE, dataCols = NULL){
+	
+	expFiles = getAE(accession, path, type = "raw")
+	
+#	if(!save) 
+#		on.exit(cleanupAE(expFiles))
+	
+	raweset = try(ae2bioc(mageFiles = expFiles, dataCols = dataCols))
+	
+	if(inherits(raweset, 'try-error')){
+		save = TRUE
+		raweset = NULL
+	}
+	else{
+		if(!is.list(raweset)){
+#			raweset = raweset[[1]];
+			message(paste("\n",accession," was successfully loaded into ",class(raweset),"\n"))
+		}
+		else {
+			message(paste("\n",accession," was successfully loaded into ",length(raweset)," ",unlist(lapply(raweset,function(x){class(x)})),"\n"))
+		}
+	}
+		
+	
+	if(!save) 
+		on.exit(cleanupAE(expFiles))
+	
+	return(raweset)
+}
